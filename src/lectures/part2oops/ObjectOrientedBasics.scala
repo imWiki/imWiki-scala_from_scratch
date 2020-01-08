63669df22d4b8cd4cb1961855792301a5bb63769
Vignesh <vignesh.meyyappan@rbs.co.uk>
2020-01-08 01:25:54 +0000

    class, method overloading, class parameters

--- src/lectures/part2oops/ObjectOrientedBasics.scala
+++ src/lectures/part2oops/ObjectOrientedBasics.scala
@@ -3,6 +3,8 @@ package lectures.part2oops
 object ObjectOrientedBasics extends App{
  val Person = new Person("Vignesh",28, "male")
   println(Person.gender)
+  Person.greetingFunc("Kavin")
+  Person.greetingFunc() //method overloading with different set of parameter list.
 }
 
 class Person (name: String, age: Int, val gender: String) {
@@ -12,5 +14,6 @@ class Person (name: String, age: Int, val gender: String) {
   val someNumber = 50   //body of the class
   println(s"Class Initiated: ${someNumber+1}") //this print statement is part of side effect, meaning if class is invoked this statement will be executed and printed in console
 
-  def greetingFunc(name: String) = println(s"Hello $name")
+  def greetingFunc(name: String) = println(s"${this.name} says: Welcome, $name") //'this' will invoke class parameter here instead of method argument
+  def greetingFunc() = println(s"Hi, $name") //here name is class parameter as method doesn't have any overloaded parameter
 }
