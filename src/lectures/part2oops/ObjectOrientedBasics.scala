3dbc4897a5aa69f9bf832c9d5359ad08b3ddb622
Vignesh <vignesh.meyyappan@rbs.co.uk>
2020-01-08 01:52:27 +0000

    method overloading

--- src/lectures/part2oops/ObjectOrientedBasics.scala
+++ src/lectures/part2oops/ObjectOrientedBasics.scala
@@ -5,12 +5,17 @@ object ObjectOrientedBasics extends App{
   println(Person.gender + ", Dummy Parameter:" + Person.dummy)
   Person.greetingFunc("Kavin")
   Person.greetingFunc() //method overloading with different set of parameter list.
+  Person.auxConstructor()
+
 }
 
 class Person (name: String, age: Int, val gender: String, val dummy: Int = 0) {
- // Class parameters are not accessible outside the class
- // Class fields are accessible outside the class ex: gender
- // Class parameters also allow default values
+
+  /* Class parameters are not accessible outside the class
+   * Class fields are accessible outside the class ex: gender
+   * Class parameters also allow default values
+   */
+
   val someNumber = 50   //body of the class
   println(s"Class Initiated: ${someNumber+1}") //this print statement is part of side effect, meaning if class is invoked this statement will be executed and printed in console
 
@@ -18,7 +23,12 @@ class Person (name: String, age: Int, val gender: String, val dummy: Int = 0) {
   def greetingFunc(name: String) = println(s"${this.name} says: Welcome, $name") //'this' will invoke class parameter here instead of method argument
 
   //method overloading
-  def greetingFunc() = println(s"Hi, $name") //here name is class parameter as method doesn't have any overloaded parameter
+  def greetingFunc() = println(s"Hi, $name, your age is $age") //here name is class parameter as method doesn't have any overloaded parameter
 
+  /* overloading can be done with different list of parameter or different data types of parameter with different return types
+  * only keeping different return types will not work, compiler will throw error if you uncomment below method
+  */
+
+  //def greetingFunc(): String = "Let's see if it works"
 
 }
